1f716d0e593b93bd81a95b31239b0c3741d0ff2b
Vanessa Macedo <vannessinhamacedo@gmail.com>
2026-09-23 00:05:55 -0300

    Implementar estrutura de Profile

---
+++ src/main/java/com/devshowcase/devshowcase_api/dto/profile/ProfileResponseDTO.java
@@ -0,0 +1,44 @@
+package com.devshowcase.devshowcase_api.dto.profile;
+
+public class ProfileResponseDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String bio;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
